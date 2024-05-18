451f1470b6d6fd65eff03b05bbba95e6cc7bfca0
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2024-05-19 00:06:08 +0300

    chore: Update package.json with devDependencies and start script for nodemon

--- server/app.js
+++ server/app.js
@@ -1,10 +1,41 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const cors = require("cors");
+
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+//connectDB
+const connectDB = require("./db/connect");
 
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
-});
+const mainRouter = require("./routes/routes");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+app.use(cors());
+
+// app.use("/", (req, res) => {
+//     res.send("<h1>Near East API</h1>");
+// });
+
+app.use("/api/v1", mainRouter);
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
+start();
