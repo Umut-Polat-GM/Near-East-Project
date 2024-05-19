23b1e15a1aa601da8d3fded1bd6df95866c0d3d7
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2024-05-19 23:55:29 +0300

    chore: Update theme and styles for App and index.css files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,49 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import { useSelector } from "react-redux";
 import Register from "./pages/Register";
+import { ThemeProvider, createTheme } from "@mui/material";
+import Profile from "./pages/Profile";
+
+const theme = createTheme({
+    palette: {
+        secondary: {
+            main: "#8E2947",
+        },
+    },
+    components: {
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    "& .MuiOutlinedInput-notchedOutline": {
+                        border: "none",
+                    },
+                    "&:hover .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#8E2947",
+                    },
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#8E2947",
+                    },
+                },
+            },
+        },
+    },
+});
 
 const App = () => {
     const userInfo = useSelector((state) => state.user.user);
     // console.log("userInfo", userInfo);
     return (
-        <Box className="bg-white min-h-screen text-black">
-            <Notifications />
-            <Routes>
-                <Route path="/" element={userInfo ? <Home /> : <Navigate to="/login" />} />
-                <Route path="/login" element={userInfo ? <Navigate to="/" /> : <Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </Box>
+        <ThemeProvider theme={theme}>
+            <Box className="bg-white min-h-screen">
+                <Notifications />
+                <Routes>
+                    <Route path="/" element={userInfo ? <Home /> : <Navigate to="/login" />} />
+                    <Route path="/profile" element={userInfo ? <Profile /> : <Navigate to="/login" />} />
+                    <Route path="/login" element={userInfo ? <Navigate to="/" /> : <Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Box>
+        </ThemeProvider>
     );
 };
 
